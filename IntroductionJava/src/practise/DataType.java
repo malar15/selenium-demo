deae4f80b7ea8ed466c352c412d0b8a6bc86cc26
Malarvizhi <fleur.lalithaa@gmail.com>
2020-01-21 09:12:51 -0800

    DataTypes

---
+++ IntroductionJava/src/practise/DataType.java
@@ -0,0 +1,32 @@
+package practise;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Primitive datatype in java");
+		
+		int i=10;
+		char c= 'c';
+        long l= 1029656789;
+        short s= 1000;
+        double d= 2.34;
+        float f=1234576f;
+        boolean b= true;
+        byte by= 12;
+        String st= "Hello";
+        
+        
+        System.out.println("the value of interger is "+ i);
+		System.out.println("the value of char is" +c);
+		System.out.println("the value of long is "+ l);
+        System.out.println("the value of short is "+ s);
+        System.out.println("the value of double is "+ d);
+        System.out.println("the value of double is " +f);
+        System.out.println("the value of double is "+ b);
+        System.out.println("the value of double is "+ by);
+        System.out.println("the value of double is "+ st);
+
+	}
+
+}
