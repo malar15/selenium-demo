3fc03863191bf819e7978da9588d3474c4052b49
Malarvizhi <fleur.lalithaa@gmail.com>
2020-01-21 21:22:54 -0800

    Updated Changes

--- Selenium/src/seleniumTest/MouseHandling.java
+++ Selenium/src/seleniumTest/MouseHandling.java
@@ -17,15 +17,18 @@ public class MouseHandling {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/saravmalar/SeleniumSoftwares/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.hdfcbank.com/");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		//driver.findElement(By.xpath("//*[@id=\"custom-search-input\"]/div/div/div/div/input")).sendKeys("abc");
+		driver.get("https://www.wellsfargo.com/");
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		//driver.findElement(By.xpath("//*[@id=\"custom-search-input\"]/div/div/div/div/input")).sendKeys("abc");
+//		
+//		WebElement a =driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[13]/div[2]/div[3]/div/div/div/div/div/div/div/div[1]/div[2]/ul/li[2]/a"));
+//		WebElement b = driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[13]/div[2]/div[3]/div/div/div/div/div/div/div/div[1]/div[2]/ul/li[2]/ul/li[2]/ul/li[2]/a/span"));
 		
-		WebElement a =driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[13]/div[2]/div[3]/div/div/div/div/div/div/div/div[1]/div[2]/ul/li[2]/a"));
-		WebElement b = driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[13]/div[2]/div[3]/div/div/div/div/div/div/div/div[1]/div[2]/ul/li[2]/ul/li[2]/ul/li[2]/a/span"));
+		WebElement a = driver.findElement(By.xpath("//a[@id='bankingTab']"));
 		Actions action = new Actions(driver);
-		action.moveToElement(a);
-		action.moveToElement(b).click().build().perform();
+		action.moveToElement(a).click().build().perform();;
+		driver.close();
+		
 		
 	
 	}
