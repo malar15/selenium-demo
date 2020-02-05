45d106d452ceba95634fa5d455f46b072b454a94
Malarvizhi <fleur.lalithaa@gmail.com>
2020-02-05 14:13:03 -0800

    Ant Build Example

---
+++ Sample/src/practise/Sample.java
@@ -0,0 +1,11 @@
+package practise;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello Javatpoint");  
+
+	}
+
+}
